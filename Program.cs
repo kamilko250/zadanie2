d4a91fa4f86e1e3f95f4461b6e5f063cf8d30e13
Fresh2054 <freshmultikk21@gmail.com>
2021-04-27 21:02:36 +0200

    zadanie

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace zadanie2
+{
+    class Program
+    {
+        class Solution
+        {
+            public static int[] solution(int[] tab, int rotations)
+            {
+                for (int j = 0; j < rotations; j++)
+                {
+                    int bufor = tab[tab.Length - 1];
+                    for (int i = tab.Length - 1 ; i > 0; i--)
+                    {
+                        tab[i] = tab[i - 1];
+                    }
+                    tab[0] = bufor;
+
+                }
+                return tab;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int[] tab = { 3, 8, 9, 7, 6 };
+            int r = 3;
+            tab = Solution.solution(tab, r);
+            for(int i = 0; i < tab.Length; i++)
+            {
+                Console.Write($"{tab[i] } ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
